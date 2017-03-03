072acb8c056c919b8fe597bd30789e65376639f4
Todd Anglin <toddanglin@gmail.com>
2017-03-03 16:54:16 -0600

    Updated with new methods in Sonos.ts

--- index.d.ts
+++ index.d.ts
@@ -10,7 +10,7 @@ export declare class Sonos {
     pause: () => Promise<boolean>;
     flush: () => Promise<any>;
     becomeCoordinatorOfStandaloneGroup: () => Promise<boolean>;
-    seek: (seconds: any, callback: any) => Promise<boolean>;
+    seek: (seconds: number) => Promise<boolean>;
     next: () => Promise<boolean>;
     previous: () => Promise<boolean>;
     setName: (name: any) => Promise<any>;
@@ -21,9 +21,22 @@ export declare class Sonos {
     selectQueue: () => Promise<boolean>;
     queue: (uri: string | UriObject, positionInQueue?: number) => Promise<any>;
     getCurrentState: () => Promise<SonosState>;
-    getZoneInfo: () => Promise<any>;
-    getZoneAttrs: () => Promise<any>;
+    getZoneInfo: () => Promise<ZoneInfo>;
+    getZoneAttrs: () => Promise<ZoneAttributes>;
+    getLEDState: () => Promise<boolean>;
+    setLEDState: (setStateOn: boolean) => Promise<void>;
+    getTopology: () => Promise<SonosTopology>;
     deviceDescription: () => Promise<any>;
+    getMusicLibrary: (searchType: SonosSearchType, startIndex?: number, pageSize?: number) => Promise<SearchMusicResult>;
+    searchMusicLibrary: (searchType: SonosSearchType, searchTerm: string, startIndex?: number, pageSize?: number) => Promise<SearchMusicResult>;
+    getFavoritesRadioStations: (startIndex?: number, pageSize?: number) => Promise<SearchMusicResult>;
+    getFavoritesRadioShows: (startIndex?: number, pageSize?: number) => Promise<SearchMusicResult>;
+    getFavoritesRadio: (favoriteRadioType: any, startIndex?: number, pageSize?: number) => Promise<SearchMusicResult>;
+    getQueue: () => Promise<SearchMusicResult>;
+    playTuneinRadio: (stationId: any, stationTitle: any) => Promise<boolean>;
+    addSpotify: (trackId: any) => Promise<any>;
+    playSpotifyRadio: (artistId: any, artistName: any) => Promise<boolean>;
+    addSpotifyQueue: (trackId: any) => Promise<any>;
 }
 
 export declare class Track {
@@ -35,12 +48,52 @@ export declare class Track {
     title: string;
     artist: string;
     album: string;
-    constructor(position: number, duration: number, albumArtUrl?: string, albumArtUri?: string, uri?: string, title?: string, artist?: string, album?: string);
+    constructor(position?: number, duration?: number, albumArtUrl?: string, albumArtUri?: string, uri?: string, title?: string, artist?: string, album?: string);
 }
 export declare class UriObject {
     uri: string;
     metadata: any;
     constructor(uri: string, metadata: any);
+}
+export declare class ZoneAttributes {
+    currentZoneName: string;
+    currentIcon: string;
+    currentConfiguration: string;
+    constructor(currentZoneName?: string, currentIcon?: string, currentConfiguration?: string);
+}
+export declare class ZoneInfo {
+    serialNumber: string;
+    softwareVersion: string;
+    displaySoftwareVersion: string;
+    hardwareVersion: string;
+    ipAddress: string;
+    macAddress: string;
+    copyrightInfo: string;
+    extraInfo: any;
+    htAudioIn: boolean;
+    flags: boolean;
+    constructor(serialNumber?: string, softwareVersion?: string, displaySoftwareVersion?: string, hardwareVersion?: string, ipAddress?: string, macAddress?: string, copyrightInfo?: string, extraInfo?: any, htAudioIn?: boolean, flags?: boolean);
+}
+export declare class SearchMusicResult {
+    returned: number;
+    total: number;
+    items: Array<Track>;
+    constructor(returned: number, total: number, items: Array<Track>);
+}
+export declare class SonosTopology {
+    zones: any;
+    mediaServers: any;
+    constructor(zones?: any, mediaServers?: any);
+}
+export declare enum SonosSearchType {
+    artists = 0,
+    albumArtists = 1,
+    albums = 2,
+    genres = 3,
+    composers = 4,
+    tracks = 5,
+    playlists = 6,
+    share = 7,
 }
 export declare enum PlayModeEnum {
     NORMAL = 0,
@@ -55,4 +108,3 @@ export declare enum SonosState {
     TRANSITIONING = 3,
     NO_MEDIA_PRESENT = 4,
 }
-
